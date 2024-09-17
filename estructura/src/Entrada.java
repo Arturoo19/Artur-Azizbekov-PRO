7be88339f250e41b14845a9c9512c28bc40ffe86
Artur <artur.lutsk.19@gmail.com>
2024-09-17 13:53:08 +0200

    updete

---
+++ estructura/src/Entrada.java
@@ -0,0 +1,11 @@
+public class Entrada {
+    public static void main(String[] args) {
+        System.out.println("Tengo 18 anos");
+        System.out.println("Primer proyecto java");
+        System.out.println("Me llamo Artur");
+        //TODO
+
+
+    }
+
+}
