85d8f7d1aa258d4e4e28b2592a49df186b9f5472
Artur <artur.lutsk.19@gmail.com>
2024-10-15 10:14:49 +0200

    update

--- Switch/src/Switch.java
+++ Switch/src/Switch.java
@@ -4,7 +4,12 @@ public class Switch {
 
     public static void main(String[] args) {
         //ejercicio1();
-        ejercicio4();
+        //ejercicio4();
+        //ejercicio6();
+        //ejercicio7();
+        //ejercicio8();
+        //ejercicio9();
+        //ejercicio10();
     }
     public static void ejercicio1(){
         System.out.println("Introduce el numero:");
@@ -73,5 +78,105 @@ public class Switch {
                 break;
 
         }
+    }
+    public static void ejercicio6(){
+        System.out.println("Ingesa un numero de 1 a 4:");
+        int numero = lectorTeclado.nextInt();
+
+        switch (numero){
+            case 1:
+                System.out.println("Opcion1 seleccionada");
+                break;
+            case 2:
+                System.out.println("Opcion2 seleccionasa");
+                break;
+            case 3:
+                System.out.println("Opcion3 seleccionada");
+                break;
+            case 4:
+                System.out.println("Opcion4 seleccionada");
+                break;
+
+        }
+    }
+    public static void ejercicio7(){
+        System.out.println("Ingrese una nota de 1 al 5:");
+        int numero = lectorTeclado.nextInt();
+
+        switch (numero){
+            case 1,2:
+                System.out.println("Suspenso");
+                break;
+            case 3:
+                System.out.println("Aprobado");
+                break;
+            case 4:
+                System.out.println("Notable");
+                break;
+            case 5:
+                System.out.println("Sobresaliente");
+                break;
+        }
+    }
+    public static void ejercicio8(){
+        System.out.println("Ingresa primer numero");
+        int numero1 = lectorTeclado.nextInt();
+        System.out.println("Ingresa segundo numero");
+        int numero2 = lectorTeclado.nextInt();
+
+        System.out.println("Ingresa una operacion matematica(+,-,*,/)");
+        char operacion = lectorTeclado.next().charAt(0);
+        int resultado=0;
+        switch (operacion){
+            case '+':
+                resultado=numero1+numero2;
+                break;
+            case '-':
+                resultado=numero1-numero2;
+                break;
+            case '*':
+                resultado=numero1*numero2;
+                break;
+            case '/':
+                resultado=numero1/numero2;
+                break;
+        }
+        System.out.println("Resultado es:"+resultado);
+
+    }
+    public static void ejercicio9(){
+        System.out.println("Ingrese un numero de 1 al 12:");
+        int estacion = lectorTeclado.nextInt();
+        switch (estacion){
+            case 1,2,12:
+                System.out.println("Invierno");
+                break;
+            case 3,4,5:
+                System.out.println("Primavera");
+                break;
+            case 6,7,8:
+                System.out.println("Verano");
+                break;
+            case 9,10,11:
+                System.out.println("Otoño");
+                break;
+        }
+    }
+    public static void ejercicio10(){
+        System.out.println("Ingrese el codigo del producto");
+        int codigo = lectorTeclado.nextInt();
+
+        switch (codigo){
+            case 1:
+                System.out.println("Con el codigo 1 tenemos Samsung s24 con el precio 800$");
+                break;
+            case 2:
+                System.out.println("Con el codigo 2 tenemos Play5 con precio 500$");
+                break;
+            case 3:
+                System.out.println("Con el codigo 3 tenemos iPhone 16 con el precio 800");
+                break;
+        }
+
     }
 }
---
+++ for/src/Ejercicios.java
@@ -0,0 +1,210 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.io.FilterOutputStream;
+import java.util.Scanner;
+import java.util.Random;
+public class Ejercicios {
+    static Scanner lectorTeclado = new Scanner(System.in);
+    static Random random = new Random();
+
+    public static void main(String[] args) {
+        //ejercicio1();
+        //ejercicio2();
+        //ejercicio3();
+        //ejercicio4();
+        //ejercicio5();
+        //ejercicio6();
+        //ejercicio7();
+        //ejercicio8();
+        //ejercicio9();
+        //ejercicio10();
+        //ejercicio11();
+        //ejercicio12();
+        ejercicio13();
+    }
+    public static void ejercicio1(){
+        System.out.println("Introduce cuantos numeros quieres introducir");
+        int numeros = lectorTeclado.nextInt();
+        double suma = 0;
+
+        if (numeros<=0){
+            numeros=10;
+        }
+        for (int i=0;i<numeros;i++){
+            System.out.println("Introduce numero:");
+            int temperatura = lectorTeclado.nextInt();
+            suma += temperatura;
+        }
+        double media = suma/numeros;
+        System.out.printf("La media es:%.2f",media);
+    }
+    public static void ejercicio2(){
+        System.out.println("Introduce numero");
+        int numero = lectorTeclado.nextInt();
+
+        for (int i=0; i<11;i++){
+            System.out.printf("%d * %d=%d\n",numero,i,numero*i);
+        }
+    }
+    public static void ejercicio3(){
+        for (int i=1; i<11;i++){
+            for (int n=1;n<11;n++){
+                int multiplicacion=i*n;
+                System.out.printf("%d * %d= %d\n",i,n,multiplicacion);
+            }
+        }
+    }
+    public static void ejercicio4(){
+        System.out.println("Escribe desde que numero quieres multiplicar:");
+        int numero1 = lectorTeclado.nextInt();
+        System.out.println("Escribe hasta que numero quieres multiplicar:");
+        int numero2 = lectorTeclado.nextInt();
+
+        for (int i=numero1; i<=numero2;i++){
+            for (int n=1;n<10;n++){
+                int multiplicacion=i*n;
+                System.out.printf("%d * %d= %d\n",i,n,multiplicacion);
+            }
+        }
+    }
+    public static void ejercicio5(){
+        System.out.println("Introduca la base");
+        int base = lectorTeclado.nextInt();
+        System.out.println("Introduce exponente");
+        int exponente = lectorTeclado.nextInt();
+        double resultado = 1;
+
+        if (base==0 && exponente==0){
+            System.out.println("Error");
+        } else if (exponente==0) {
+            System.out.println(1);
+        } else {
+            for (int i = 1; i <= exponente; i++) {
+                resultado *= base;
+            }
+            System.out.println(resultado);
+        }
+    }
+    public static void ejercicio6(){
+        System.out.println("Introduce primer numero:");
+        int numero1 = lectorTeclado.nextInt();
+        System.out.println("Introduce segundo numero:");
+        int numero2 = lectorTeclado.nextInt();
+        for (int i=numero1;i<=numero2;i++){
+            if (i%2==0){
+                System.out.println(i);
+            }
+        }
+
+    }
+    public static void ejercicio7(){
+        System.out.println("Introduce cuantas veces quieres simular lanzamiento de un dado:");
+        int dadoVeces = lectorTeclado.nextInt();
+        if (dadoVeces<=0){
+            dadoVeces = 100;
+        }
+        for (int i=0;i<dadoVeces;i++){
+            int resultado = random.nextInt(1,6);
+            System.out.println("Resultado de un dado es:"+resultado);
+        }
+    }
+    public static void ejercicio8(){
+        double positivos = 0;
+        int cantidadPositivos=0;
+        int cantidadCeros = 0;
+        double negativos = 0;
+        int cantidadNegativos=0;
+        for (int i=0;i<10;i++){
+            System.out.println("Escribe un numero:");
+            int numero = lectorTeclado.nextInt();
+            if (numero>0){
+                positivos+=numero;
+                cantidadPositivos++;
+            } else if (numero==0) {
+                cantidadCeros++;
+            } else {
+                negativos+=numero;
+                cantidadNegativos++;
+            }
+
+        }
+        System.out.println("Cantidad de ceros:"+cantidadCeros);
+        double mediaPositivos = positivos/cantidadPositivos;
+        System.out.printf("Media de positivos:%.2f\n",mediaPositivos);
+        double mediaNegativos = negativos/cantidadNegativos;
+        System.out.printf("Media de negativos:%.2f",mediaNegativos);
+
+    }
+    public static void ejercicio9(){
+        int sumaSueldos = 0;
+        int mayoresDe1000 = 0;
+
+        for (int i=0;i<10;i++){
+            System.out.println("Introduce sueldo:");
+            int sueldo = lectorTeclado.nextInt();
+            sumaSueldos+=sueldo;
+            if (sueldo>1000){
+                mayoresDe1000++;
+            }
+        }
+        double media = (double) sumaSueldos /10;
+        System.out.println("Suma sueldos:"+sumaSueldos);
+        System.out.println("Media sueldos:"+media);
+        System.out.println("Cuantos mayores de 1000:"+mayoresDe1000);
+    }
+    public static void ejercicio10(){
+        int aprobados = 0;
+        int suspendidos = 0;
+        int condicionados = 0;
+        for (int i=0;i<6;i++){
+            System.out.println("Intoduce nota:");
+            int nota = lectorTeclado.nextInt();
+            if (nota>4){
+                aprobados++;
+            } else if (nota==4) {
+                condicionados++;
+            } else {
+                suspendidos++;
+            }
+        }
+        System.out.println("Aprobados:"+aprobados);
+        System.out.println("Condicionados"+condicionados);
+        System.out.println("Suspendidos"+suspendidos);
+    }
+    public static void ejercicio11(){
+        System.out.println("Escribe tamaño de cuadrado:");
+        int lado = lectorTeclado.nextInt();
+        for (int i=0;i<lado;i++){
+            for (int j=0; j<lado; j++){
+                System.out.print("+");
+            }
+            System.out.println();
+        }
+    }
+    public static void ejercicio12(){
+        System.out.println("Escribe tamaño de cuadrado:");
+        int lado = lectorTeclado.nextInt();
+        for (int i=0;i<lado;i++){
+            for (int j=0; j<lado; j++){
+                if (i==0|| i == lado-1|| j==0||j==lado-1){
+                    System.out.print("-");
+                }else {
+                    System.out.print("+");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void  ejercicio13(){
+        System.out.println("Introduce una palabra: ");
+        String palabra = lectorTeclado.next();
+        String letra = "";
+
+        for (int i = palabra.length() -1; i >= 0; i--) {
+            letra += palabra.charAt(i);
+        }
+        System.out.println("Palabra inversa es: "+letra);
+
+    }
+
+}
