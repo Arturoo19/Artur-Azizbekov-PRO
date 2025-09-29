6d861c348909cd8d4e58a4db6694e60d26a5c9ef
Artur <132137116+Arturoo19@users.noreply.github.com>
2025-09-29 13:33:46 +0200

    .

--- DAM2/TodoList/src/model/Encargo.java
+++ DAM2/TodoList/src/model/Encargo.java
@@ -1,19 +1,26 @@
 package model;
 
+/*
+    Un encargo debe tener un responsable
+ */
 public class Encargo {
-
     private int id;
     private String descripcion;
     private boolean completada;
 
+    public Encargo(int id, String descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+        // completada = false;
+    }
 
     public Encargo() {
     }
 
-    public Encargo(int id, String descripcion, boolean completada) {
-        this.id = id;
-        this.descripcion = descripcion;
-        this.completada = completada;
+    public void mostrarDatos(){
+        System.out.println("id = " + id);
+        System.out.println("descripcion = " + descripcion);
+        System.out.println("completada = " + completada);
     }
 
     public int getId() {
@@ -38,5 +45,14 @@ public class Encargo {
 
     public void setCompletada(boolean completada) {
         this.completada = completada;
+    }
+
+    @Override
+    public String toString() {
+        return "Encargo{" +
+                "id=" + id +
+                ", descripcion='" + descripcion + '\'' +
+                ", completada=" + completada +
+                '}';
     }
 }
